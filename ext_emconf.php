fdd80830401bd33a998009a7c2ebe6e27dff24eb
Michael Paffrath <michael.paffrath@gmail.com>
2022-02-12 23:13:56 +0100

    Update emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,12 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'ce_kickstarter',
-    'description' => 'asdf',
+    'description' => 'A kickstarter for content elements',
+    'category' => 'misc',
+    'author' => 'Michael Paffrath',
+    'author_email' => 'michael.paffrath@gmail.com',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
